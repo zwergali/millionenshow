e3fe3bf20bdc085e9abb6c06c987b794efe6322f
Marcel Wimmer <marcelwimmerzwergali@gmail.com>
2025-12-07 10:55:18 +0100

    "fertig"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,17 +19,20 @@
 </script>
 
 <main>
-	<h1>{frage.frage}</h1>
-	<div class="buttons">
-		{#each [0, 1, 2, 3] as index}
-			<button
-				onclick={() => onButtonClick(index)}
-				class={falschGewählteAntworten.includes(index) ? 'ungültig' : ''}
-			>
-				{frage.antworten[index]}
-			</button>
-		{/each}
+	<div class="box">
+		<h1>{frage.frage}</h1>
+		<div class="buttons">
+			{#each [0, 1, 2, 3] as index}
+				<button
+					onclick={() => onButtonClick(index)}
+					class={falschGewählteAntworten.includes(index) ? 'ungültig' : ''}
+				>
+					{frage.antworten[index]}
+				</button>
+			{/each}
+		</div>
 	</div>
+	<div class="bg"></div>
 </main>
 
 <style>
@@ -39,7 +42,17 @@
 		width: 100%;
 		min-height: 100%;
 		padding: 20px;
+		position: relative;
+		justify-content: flex-end;
+	}
+
+	.box {
+		display: flex;
+		flex-direction: column;
 		gap: 40px;
+		background-color: #a3a3a391;
+		border-radius: 16px;
+		padding: 20px;
 	}
 
 	h1 {
@@ -61,5 +74,56 @@
 
 	.ungültig {
 		background-color: red;
+	}
+
+	.bg {
+		overflow: hidden;
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		left: 0;
+		z-index: -1;
+
+		container-type: size;
+	}
+
+	.bg::after {
+		content: '';
+		display: block;
+		height: 200cqmax;
+		width: 200cqmax;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		translate: -50% -50%;
+		background-size: 100% 100%;
+		background-position: 0px 0px;
+		animation: rotation 40s linear 0s infinite forwards;
+
+		--c1: hsl(117 58% 38%);
+		--c2: hsl(233 100% 42%);
+		background-image:
+			radial-gradient(#c7cad6, #c7cad6 20px, #c7cad600 100px),
+			conic-gradient(
+				var(--c1),
+				var(--c2),
+				var(--c1),
+				var(--c2),
+				var(--c1),
+				var(--c2),
+				var(--c1),
+				var(--c2),
+				var(--c1)
+			);
+	}
+
+	@keyframes rotation {
+		0% {
+			rotate: 0deg;
+		}
+		100% {
+			rotate: 360deg;
+		}
 	}
 </style>
